a01dfe9404abe3d744ccb9db729d7b671f561fd1
Aradhya Jain <ajain@qodeleaf.com>
2025-01-23 18:08:58 +0530

    Helpers: Functions for functionalities being used again and again in controllers

---
+++ backend/src/utils/helpers.js
@@ -0,0 +1,29 @@
+import User from '../models/user.js';
+
+export const catchAsync = (fn) => (req, res, next) => {
+  fn(req, res, next).catch(next);
+};
+
+export const ensureUserExists = async (walletAddress) => {
+  if (!walletAddress) {
+    throw new Error('Wallet address is required');
+  }
+
+  const normalizedAddress = walletAddress.toLowerCase();
+
+  let user = await User.findOne({ walletAddress: normalizedAddress });
+
+  if (!user) {
+    user = await User.create({ walletAddress: normalizedAddress });
+  }
+
+  return user._id;
+};
+
+export const handleBlockchainTransaction = async (tx) => {
+  const receipt = await tx.wait();
+  if (receipt.status !== 1) {
+    throw new Error('Blockchain transaction failed.');
+  }
+  return receipt;
+};
