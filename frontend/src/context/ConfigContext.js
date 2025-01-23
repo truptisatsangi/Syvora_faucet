d4a430865c476129d2a7cd78c892509c40134c38
Aradhya Jain <ajain@qodeleaf.com>
2025-01-23 18:25:06 +0530

    Provider: Configuration provider for making availability of environment specific backend URLs for fetch requests

---
+++ frontend/src/context/ConfigContext.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+import { getConfig } from "../config/config";
+
+const ConfigContext = createContext(getConfig());
+
+export const ConfigProvider = ({ children }) => {
+  const config = getConfig();
+  return (
+    <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
+  );
+};
+
+export const useConfig = () => useContext(ConfigContext);
