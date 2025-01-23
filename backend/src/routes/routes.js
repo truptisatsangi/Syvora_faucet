a93119f8ca2af8d91a381084b270301799abff58
Aradhya Jain <ajain@qodeleaf.com>
2025-01-23 18:06:51 +0530

    API Endpoint Routes with their controller methods

---
+++ backend/src/routes/routes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+    borrowFaucet,
+    lendFaucet,
+    updateWhitelist,
+    withdrawFunds,
+    signUp,
+    signIn,
+    getWalletAddressBalance, 
+    getTreasuryBalance,
+    checkOwner
+} from "../controllers/controllers.js";
+
+const router = express.Router();
+
+router.post("/borrow", borrowFaucet);
+router.post("/lend", lendFaucet);
+router.post("/whitelist", updateWhitelist);
+router.post("/withdraw", withdrawFunds);
+
+router.post('/balance/walletAddress', getWalletAddressBalance);
+router.get('/balance/treasury', getTreasuryBalance);
+
+router.post("/auth/signup", signUp);
+router.post("/auth/signin", signIn);
+
+router.post("/isOwner", checkOwner);
+
+export default router;
