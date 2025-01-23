2a828c53094d8927603f4160460d9db694cef6bb
Aradhya Jain <ajain@qodeleaf.com>
2025-01-23 18:25:59 +0530

    Provider: Wallet Provider for making availability of variables and functions for connecting to browser web3 provider

---
+++ frontend/src/context/WalletContext.js
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState } from "react";
+import { ethers } from "ethers";
+
+const WalletContext = createContext();
+
+export const WalletProvider = ({ children }) => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [loading, ] = useState(false);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+
+  // const handleAccountsChanged = (accounts) => {
+  //   if (accounts.length > 0) {
+  //     setIsConnected(true);
+  //     setAccount(accounts[0]);
+  //   } else {
+  //     setIsConnected(false);
+  //     setAccount(null);
+  //   }
+  // };
+
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setIsConnected(true);
+        setAccount(accounts[0]);
+
+        const browserProvider = new ethers.BrowserProvider(window.ethereum);
+        const userSigner = await browserProvider.getSigner();
+        
+        setProvider(browserProvider);
+        setSigner(userSigner);
+      } catch (error) {
+        console.error("Error connecting to MetaMask", error);
+      }
+    } else {
+      alert("MetaMask is not installed!");
+    }
+  };
+
+  const disconnectWallet = () => {
+    setIsConnected(false);
+    setAccount(null);
+    setProvider(null);
+    setSigner(null);
+  };
+
+  return (
+    <WalletContext.Provider
+      value={{
+        isConnected,
+        account,
+        connectMetaMask,
+        disconnectWallet,
+        loading,
+        provider,
+        signer,
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  );
+};
+
+export const useWallet = () => useContext(WalletContext);
