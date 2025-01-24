8af2127160beabfb68fee1bc2c1e5f3590ed73bc
Aradhya Jain <ajain@qodeleaf.com>
2025-01-24 15:51:57 +0530

    Component: Alert component for unauthorized access

---
+++ frontend/src/components/alerts/UnauthorizedAlert.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Alert, AlertDescription } from "../../components/ui/alert";
+
+const UnauthorizedAlert = () => (
+  <Alert variant="destructive">
+    <AlertDescription>You are not authorized to access this page.</AlertDescription>
+  </Alert>
+);
+
+export default UnauthorizedAlert;
