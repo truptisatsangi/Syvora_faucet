329b1e752f0fc10a3514bf4419eb4d876028adc1
Aradhya Jain <ajain@qodeleaf.com>
2025-01-24 15:51:30 +0530

    Components: Buttons for google, github signin and theme toggle button

---
+++ frontend/src/components/buttons/OAuthButtons.tsx
@@ -0,0 +1,11 @@
+import GoogleSignInButton from '../buttons/GoogleSignInButton';
+import GithubSignInButton from '../buttons/GithubSignInButton';
+
+const OAuthButtons = () => (
+  <div className="w-full space-y-4">
+    <GoogleSignInButton />
+    <GithubSignInButton />
+  </div>
+);
+
+export default OAuthButtons;
---
+++ frontend/src/components/buttons/ToggleThemeButton.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import * as React from "react"
+import { Toggle } from "../../components/ui/toggle"
+
+export function ToggleThemeButton() {
+  const { theme, setTheme } = useTheme()
+
+  const isDarkMode = theme === "dark"
+
+  return (
+    <Toggle
+      aria-label="Toggle theme"
+      pressed={isDarkMode}
+      onClick={() => setTheme(isDarkMode ? "light" : "dark")}
+    >
+      {isDarkMode ? (
+        <Sun className="h-5 w-5 text-white" />
+      ) : (
+        <Moon className="h-5 w-5 text-black" />
+      )}
+    </Toggle>
+  )
+}
