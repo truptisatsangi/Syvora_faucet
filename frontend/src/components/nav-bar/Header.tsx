de052ca41d1614d49d5a178ee5aa30517824c624
Aradhya Jain <ajain@qodeleaf.com>
2025-01-28 15:12:23 +0530

    Refactor: Header components visibility on different path routes

--- frontend/src/components/nav-bar/Header.tsx
+++ frontend/src/components/nav-bar/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,16 +5,16 @@ import { usePathname, useRouter } from "next/navigation";
 import { useAuth } from "../../context/AuthContext";
 import { useWallet } from "../../context/WalletContext";
 import { ToggleThemeButton } from "../../components/buttons/ToggleThemeButton";
-import { Button } from "../../components/ui/button";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from "../../components/ui/dropdown-menu";
-import { Spinner } from "../../components/ui/spinner";
+import { WalletDropdown } from "../../components/WalletDropdown";
+import { NavButton } from "../../components/buttons/NavButton";
+import { Button } from "../ui/button";
 import { useOwnerCheck } from "../../hooks/useOwnerCheck";
 import { useWalletBalance } from "../../hooks/useWalletBalance";
 
 export default function Header() {
   const { theme } = useTheme();
   const { isConnected, account, connectMetaMask, disconnectWallet } = useWallet();
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
   const pathname = usePathname();
   const router = useRouter();
   const isDarkMode = theme === "dark";
@@ -24,20 +22,12 @@ export default function Header() {
   const { balance: walletBalance, loading } = useWalletBalance();
   const { isOwner, isCheckingOwner, checkOwner } = useOwnerCheck();
 
-  const isActive = (path: string) => pathname === path;
-
-  const navigateTo = (path: string) => {
-    if (pathname !== path) {
-      router.push(path);
-    }
-  };
-
   const isAuthPage = pathname === "/signin" || pathname === "/signup";
 
   if (account && !isCheckingOwner && isOwner === undefined) {
     checkOwner();
   }
-
+  console.log("user:- ", user);
   return pathname !== '/' ? (
     <header
       className={`fixed top-0 left-0 right-0 z-50 h-[80px] px-8 flex items-center justify-between bg-opacity-80 ${isDarkMode ? "bg-black" : "bg-white"
@@ -60,50 +50,12 @@ export default function Header() {
       {!isAuthPage && (
         <>
           <nav className="flex items-center gap-6">
-            {["/borrow", "/lend"].map((path) => (
-              <Button
-                key={path}
-                variant="ghost"
-                onClick={() => navigateTo(path)}
-                className={`px-4 py-2 text-sm font-semibold rounded-lg transition-all ease-in-out transform ${isActive(path)
-                  ? isDarkMode ? `bg-primary text-black` : `bg-primary text-white`
-                  : isDarkMode
-                    ? "text-gray-400 hover:text-white hover:scale-105"
-                    : "text-gray-600 hover:text-black hover:scale-105"
-                  }`}
-              >
-                {path.replace("/", "").toUpperCase()}
-              </Button>
-            ))}
-
+            {user && account && <NavButton path="/borrow" label="Borrow" />}
+            <NavButton path="/lend" label="Lend" />
             {isOwner && (
               <>
-                <Button
-                  key="/whitelist"
-                  variant="ghost"
-                  onClick={() => navigateTo("/whitelist")}
-                  className={`px-4 py-2 text-sm font-semibold rounded-lg transition-all ease-in-out transform ${isActive("/whitelist")
-                    ? isDarkMode ? `bg-primary text-black` : `bg-primary text-white`
-                    : isDarkMode
-                      ? "text-gray-400 hover:text-white hover:scale-105"
-                      : "text-gray-600 hover:text-black hover:scale-105"
-                    }`}
-                >
-                  WHITELIST
-                </Button>
-                <Button
-                  key="/dashboard"
-                  variant="ghost"
-                  onClick={() => navigateTo("/dashboard")}
-                  className={`px-4 py-2 text-sm font-semibold rounded-lg transition-all ease-in-out transform ${isActive("/dashboard")
-                    ? isDarkMode ? `bg-primary text-black` : `bg-primary text-white`
-                    : isDarkMode
-                      ? "text-gray-400 hover:text-white hover:scale-105"
-                      : "text-gray-600 hover:text-black hover:scale-105"
-                    }`}
-                >
-                  DASHBOARD
-                </Button>
+                <NavButton path="/whitelist" label="Whitelist" />
+                <NavButton path="/dashboard" label="Dashboard" />
               </>
             )}
           </nav>
@@ -111,57 +63,34 @@ export default function Header() {
           <div className="flex items-center gap-4">
             <ToggleThemeButton />
             {isConnected && account ? (
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="outline"
-                    className="flex items-center gap-4 shadow-lg px-6 py-2 rounded-full transition-all transform hover:scale-105"
-                  >
-                    <Image
-                      src={`/wallet_balance_${isDarkMode ? "colored" : "outline"}.png`}
-                      alt="Wallet Balance"
-                      width={20}
-                      height={20}
-                    />
-                    {loading ? (
-                      <Spinner className="ml-2" size="sm" />
-                    ) : (
-                      `${Number(walletBalance).toPrecision(3)} ETH`
-                    )}
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem disabled>
-                    Balance: {loading ? <Spinner size="sm" /> : `${Number(walletBalance).toPrecision(3)} ETH`}
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem
-                    onClick={disconnectWallet}
-                    className="text-red-600"
-                  >
-                    Disconnect
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <WalletDropdown
+                walletBalance={(walletBalance as unknown as number) || 0}
+                loading={loading}
+                isDarkMode={isDarkMode}
+                disconnectWallet={disconnectWallet}
+              />
             ) : (
-              <Button
-                variant="outline"
-                onClick={connectMetaMask}
-              >
+              <Button variant="outline" onClick={connectMetaMask}>
                 CONNECT
               </Button>
             )}
-            <Button
-              variant="destructive"
-              onClick={signOut}
-            >
+            {user && <Button variant="destructive" onClick={signOut}>
               Logout
             </Button>
+            }
+            {!user && pathname === '/lend' ? <Button variant="default" onClick={() => router.push('/signin')}>
+              Login
+            </Button> : null}
           </div>
         </>
       )}
 
-      {isAuthPage && <ToggleThemeButton />}
+      {isAuthPage && (
+        <div>
+          <NavButton path="/lend" label="Lend Tokens" />
+          <ToggleThemeButton />
+        </div>)}
+
     </header>
   ) : null;
 }
