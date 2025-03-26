cebed65acdb137009ea85d22e39d33d36864a54e
Aradhya Jain <ajain@qodeleaf.com>
2025-03-26 14:16:12 +0530

    Refactor: Modified hook to make get request for fetching wallet balance

--- frontend/src/hooks/useWalletBalance.ts
+++ frontend/src/hooks/useWalletBalance.ts
@@ -1,22 +1,36 @@
-import { ethers } from "ethers";
 import { useEffect, useState, useCallback } from "react";
 import { useWallet } from "../context/WalletContext";
+import { useConfig } from "../context/ConfigContext";
 import { useToast } from "../hooks/use-toast";
 
 export const useWalletBalance = () => {
-    const { account, provider } = useWallet();
+    const { account } = useWallet();
+    const { backendUrl } = useConfig();
     const [balance, setBalance] = useState<string>('');
     const [loading, setLoading] = useState(false);
     const { toast } = useToast();
 
     const fetchWalletBalance = useCallback(async () => {
-        if (!account || !provider) return;
+        if (!account) return;
 
         setLoading(true);
         try {
-            const balanceInWei = await provider.getBalance(account);
-            const balanceInEth = ethers.formatEther(balanceInWei);
-            setBalance(balanceInEth);
+            const response = await fetch(`${backendUrl}/balance/walletAddress?account=${account}`, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch wallet balance");
+            }
+
+            const data = await response.json();
+
+            if (!data.success) {
+                throw new Error(data.message || "Unknown error");
+            }
+
+            setBalance(data.balance);
         } catch (error: unknown) {
             if (error instanceof Error) {
                 toast({
@@ -34,7 +48,7 @@ export const useWalletBalance = () => {
         } finally {
             setLoading(false);
         }
-    }, [account, provider, toast]);
+    }, [account, backendUrl, toast]);
 
     useEffect(() => {
         fetchWalletBalance();
@@ -42,7 +56,7 @@ export const useWalletBalance = () => {
 
     const refreshWalletBalance = async () => {
         await fetchWalletBalance();
-      };
+    };
 
     return { balance, loading, refreshWalletBalance };
 };
