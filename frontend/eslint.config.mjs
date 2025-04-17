d5a4c758ad8c42a8e3ac7b0f08712338c28102cf
Aradhya Jain <ajain@qodeleaf.com>
2025-04-17 13:30:00 +0530

    feat: Plugin for auto correcting the sort order of third party libraries
    Plugin for auto correcting the sort order of third party libraries
    
    Fixed the import statements sorting

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,53 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = await (async () => {
+  const simpleImportSort = await import('eslint-plugin-simple-import-sort')
+  const eslintPluginImport = await import('eslint-plugin-import')
 
-export default eslintConfig;
+  return [
+    ...compat.extends(
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:import/recommended',
+      'plugin:import/typescript'
+    ),
+    {
+      plugins: {
+        'simple-import-sort': simpleImportSort.default,
+        import: eslintPluginImport.default
+      },
+      rules: {
+        'simple-import-sort/imports': 'warn',
+        'simple-import-sort/exports': 'warn',
+
+        'import/order': [
+          'warn',
+          {
+            groups: [
+              ['builtin', 'external'],
+              ['internal', 'parent', 'sibling', 'index', 'type']
+            ],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true
+            },
+            'newlines-between': 'always'
+          }
+        ],
+
+        '@typescript-eslint/no-require-imports': 'error'
+      }
+    }
+  ]
+})()
+
+export default eslintConfig
