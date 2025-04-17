c30e8f35b411a04fff630ad5900bd43713d5c65f
Aradhya Jain <ajain@qodeleaf.com>
2025-04-17 19:58:35 +0530

    Add: Routes for checking if user exists and get user email by wallet address

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -11,6 +11,8 @@ import {
   checkOwner,
   checkWhitelistedStatus,
   getLastBorrowedTimestamp,
+  checkUserExists,
+  getUserEmailByWallet,
 } from "../controllers/controllers.js";
 
 const router = express.Router();
@@ -22,9 +24,12 @@ router.post("/withdraw", withdrawFunds);
 
 router.post("/auth/signup", signUp);
 router.post("/auth/signin", signIn);
+router.get("/auth/userExists", checkUserExists);
 
 router.get("/balance/walletAddress", getWalletAddressBalance);
 router.get("/balance/treasury", getTreasuryBalance);
+
+router.get("/users/email/:walletAddress", getUserEmailByWallet);
 
 router.get("/isOwner", checkOwner);
 router.get("/isWhitelisted", checkWhitelistedStatus);
